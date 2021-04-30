838524ecbff650f3b34161ca2940d357a24b01c7
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 18:51:38 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,10 +1,7 @@
 <?php
 
-$link = mysqli_connect("ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432","nbvnsbswyvclsh","f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130","d7qvjv66dimcfg");
-
-if (!$link) {
-  die('Ошибка соединения: ' . mysqli_error());
-}
+$link = $conn = pg_connect(getenv("postgres://nbvnsbswyvclsh:f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/d7qvjv66dimcfg"))
+or die('Не удалось соединиться: ' . pg_last_error());
 echo 'Успешно соединились';
-mysqli_close($link);
+pg_close($link);
 ?>
