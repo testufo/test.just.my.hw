a1a2fb8f22744c3422a74f522f6364e427e7af95
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 22:16:56 +0300

    Update result.php

--- result.php
+++ result.php
@@ -9,6 +9,9 @@ unset($answerr["email"]);
 $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
     or die('Не удалось соединиться: ' . pg_last_error());
 // Выполнение SQL-запроса
+$query = "DROP TABLE answers";
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+DROP TABLE answers
 $query = 'CREATE TABLE IF NOT EXISTS answers (
   id SERIAL PRIMARY KEY,
   token CHARACTER VARYING(255),
@@ -19,7 +22,7 @@ $query = 'CREATE TABLE IF NOT EXISTS answers (
 $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
 $query = "INSERT INTO answers(token, firstname, lastname, email, answer) VALUES
-("$get["id"]", "$get["firstname"]", "$get["lastname"]", "$get["email"]", "$answerr")";
+("$get['id']", "$get['firstname']", "$get['lastname']", "$get['email']", "$answerr")";
 
 $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
