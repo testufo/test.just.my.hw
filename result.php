5c868bc4f57abc233b7f050e1c5201558d58afdb
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 19:44:08 +0300

    Update result.php

--- result.php
+++ result.php
@@ -9,8 +9,6 @@ exec('CREATE TABLE IF NOT EXISTS answers (
   firstname CHARACTER VARYING(100),
   lastname CHARACTER VARYING(100),
   email CHARACTER VARYING(100),
-  answers CHARACTER VARYING(255));');
-
-echo pg_list_tables($link);
+  answers CHARACTER VARYING(255);');
 pg_close($link);
 ?>
