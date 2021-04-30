51d727fd4cb1781ec8e2081ffed5d3f1e9b5ce1f
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 18:38:15 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = mysql_connect("ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432","nbvnsbswyvclsh","f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130");
+$link = mysqli_connect("ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432","nbvnsbswyvclsh","f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130","d7qvjv66dimcfg");
 
 if (!$link) {
   die('Ошибка соединения: ' . mysql_error());
