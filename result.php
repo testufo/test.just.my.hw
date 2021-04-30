8ddee5b126a8505b14806bfd14bc5995631d3977
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 22:53:41 +0300

    Update result.php

--- result.php
+++ result.php
@@ -10,8 +10,8 @@ $answerr = to_pg_array($answerr);
 $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
     or die('Не удалось соединиться: ' . pg_last_error());
 // Выполнение SQL-запроса
-/*$query = "DROP TABLE answers";
-$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());*/
+$query = "DROP TABLE answers";
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
 $query = 'CREATE TABLE IF NOT EXISTS answers (
   id SERIAL PRIMARY KEY,
@@ -19,7 +19,7 @@ $query = 'CREATE TABLE IF NOT EXISTS answers (
   firstname CHARACTER VARYING(100),
   lastname CHARACTER VARYING(100),
   email CHARACTER VARYING(100),
-  answer NUMBER[][])';
+  answer integer[][])';
 $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
 $query = "INSERT INTO answers(token, firstname, lastname, email, answer) VALUES
