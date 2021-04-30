2735128b36f0aaff2ffd12daaa9e2f1372a9bbea
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 20:12:58 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,20 +1,28 @@
+
 <?php
+// Соединение, выбор базы данных
+$dbconn = pg_connect("host=localhost dbname=publishing user=www password=foo")
+    or die('Не удалось соединиться: ' . pg_last_error());
 
-$link = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
-or die('Не удалось соединиться: ' . pg_last_error());
-echo 'Успешно соединились';
-pg_prepare($link, "my_query", 'CREATE TABLE IF NOT EXISTS answers (
-  id SERIAL PRIMARY KEY,
-  token CHARACTER VARYING(255),
-  firstname CHARACTER VARYING(100),
-  lastname CHARACTER VARYING(100),
-  email CHARACTER VARYING(100),
-  answers CHARACTER VARYING(255);');
-pg_execute($link, "my_query");
+// Выполнение SQL-запроса
+$query = 'SELECT * FROM authors';
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
-pg_prepare($link, "my_query", 'INSERT INTO answers(token, firstname, lastname, email, answers)
-VALUES (rdf, rdf, rdf, rdf, rdf);');
-pg_execute($link, "my_query");
+// Вывод результатов в HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
 
-pg_close($link);
+// Очистка результата
+pg_free_result($result);
+
+// Закрытие соединения
+pg_close($dbconn);
 ?>
+
