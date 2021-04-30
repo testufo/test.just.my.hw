9b351643ee41877ebeea513918ffdd774594e20c
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 18:54:54 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = $conn = pg_connect(getenv("postgres://nbvnsbswyvclsh:f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/d7qvjv66dimcfg"))
+$link = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg usernbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
 or die('Не удалось соединиться: ' . pg_last_error());
 echo 'Успешно соединились';
 pg_close($link);
