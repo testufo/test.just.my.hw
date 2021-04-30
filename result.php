636852d1d81cbb56003da329bb1e52ab69fb6c73
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 18:40:47 +0300

    Update result.php

--- result.php
+++ result.php
@@ -3,8 +3,8 @@
 $link = mysqli_connect("ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432","nbvnsbswyvclsh","f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130","d7qvjv66dimcfg");
 
 if (!$link) {
-  die('Ошибка соединения: ' . mysql_error());
+  die('Ошибка соединения: ' . mysqli_error());
 }
 echo 'Успешно соединились';
-mysql_close($link);
+mysqli_close($link);
 ?>
