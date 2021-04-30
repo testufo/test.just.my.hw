9df585a3dd576ad37ea53b62aa4762815c77ac68
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-01 02:05:14 +0300

    Update result.php

--- result.php
+++ result.php
@@ -135,7 +135,7 @@ pg_close($dbconn);
                   if($arr1!=null){
                     foreach($arr1 as $arr2){
                      // print_r('<p><a href=/?id="'.$arr2["token"].'">'.$arr2["firstname"].$arr2["lastname"].'</a></p>');
-                      print_r($arr2["token"]);
+                      print_r($arr2['token']);
                     }
                   }
                 }
