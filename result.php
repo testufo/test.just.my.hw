960753075f9cfb3b5cfe13a7dfcd354f787ee1e7
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 19:26:15 +0300

    Update result.php

--- result.php
+++ result.php
@@ -2,6 +2,7 @@
 
 $link = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
 or die('Не удалось соединиться: ' . pg_last_error());
+echo 'Успешно соединились';
 $sqlList = ['CREATE TABLE IF NOT EXISTS answers (
   id SERIAL PRIMARY KEY,
   token CHARACTER VARYING(255),
@@ -16,7 +17,5 @@ foreach ($sqlList as $sql) {
   exec($sql);
 }
 echo 'table successfully created!';
-return $this;
-echo 'Успешно соединились';
 pg_close($link);
 ?>
