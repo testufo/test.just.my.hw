7164de12c6bbd9f36f176c88f5d83dc8a4acc3a1
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 23:34:40 +0300

    Update result.php

--- result.php
+++ result.php
@@ -10,10 +10,11 @@ $answerr = json_encode($answerr);
 $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130")
     or die('Не удалось соединиться: ' . pg_last_error());
 // Выполнение SQL-запрос
-
+$query = "DROP TABLE answers";
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 $query = 'CREATE TABLE IF NOT EXISTS answers (
   id SERIAL PRIMARY KEY,
-  token CHARACTER VARYING(255),
+  token CHARACTER  UNIQUE VARYING(255),
   firstname CHARACTER VARYING(100),
   lastname CHARACTER VARYING(100),
   email CHARACTER VARYING(100),
