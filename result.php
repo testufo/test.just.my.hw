15afb670649ba658412948e8f37e93423a137180
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-04-30 18:31:06 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,26 +1,16 @@
 <?php
 
-$con = mysql_connect("mysql.cis.ksu.edu","cis_id","password");
+$con = mysql_connect("ec2-54-216-185-51.eu-west-1.compute.amazonaws.com","d7qvjv66dimcfg","f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130");
 
 if (!$con)
-
   {
-
   die('Could not connect: ' . mysql_error());
-
   }
-
- 
-
 mysql_select_db("cis_id", $con);
 
  
 
-$sql="INSERT INTO nametable (fname, lname)
-
-VALUES
-
-('$_POST[fname]','$_POST[lname]')";
+$sql="INSERT INTO nametable (fname, lname)VALUES('$_POST[fname]','$_POST[lname]')";
 
  
 
@@ -34,8 +24,5 @@ if (!mysql_query($sql,$con))
 
 echo "1 record added";
 
- 
-
 mysql_close($con)
-
 ?>
