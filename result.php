5461b7c85a70b343702b54a350ea4c0857c72f7a
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 12:49:17 +0300

    Update result.php

--- result.php
+++ result.php
@@ -60,9 +60,9 @@ pg_close($dbconn);
 <html>
     <head>
     <title> Результати </title>
-    <link href="src/styles/style.css" rel="stylesheet" type="text/css" async>
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" async></script>
-  <script id="MathJax-script" async
+    <link href="src/styles/style.css" rel="stylesheet" type="text/css" defer>
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
+  <script id="MathJax-script" defer
           src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
   </script>
     </head>
