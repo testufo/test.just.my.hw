491a02577b316b01557bb2585d2d7c1299648b9e
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 17:45:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
         </div>
       <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
   <script id="MathJax-script" defer
-          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+          src="src/js/tex-mml-chtml.js">
   </script>
     </body>
 </html>
