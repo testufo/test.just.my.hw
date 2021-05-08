20cd9218b3453b8664e41c4eaf377dd806c003ea
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 13:27:41 +0300

    Update result.php

--- result.php
+++ result.php
@@ -61,7 +61,7 @@ pg_close($dbconn);
     <head>
     <title> Результати </title>
     <meta http-equiv="Cache-Control" content="public" />
-    <link href="src/styles/style.css" rel="stylesheet" type="text/css" defer>
+    <link href="src/styles/style.css" rel="stylesheet" type="text/css" link rel="preload" as="style">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
         <script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" defer></script>
     </head>
