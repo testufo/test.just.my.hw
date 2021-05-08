eb6444818928ec1ef75580372b1d1ede55931fc1
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 15:44:44 +0300

    Update result.php

--- result.php
+++ result.php
@@ -60,17 +60,7 @@ pg_close($dbconn);
 <html>
     <head>
     <title> Результати </title>
-    <link rel="preload" href="src/styles/style.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<noscript><link rel="stylesheet" href="src/styles/style.css"></noscript>
-  <style type="text/css">
-   .content {
-      border-style:double;
-      border-radius: 5px;
-      border-width: 3px;
-      background-color: #00000085;
-      margin: 2%;
-      padding: 5px 10px;
-  }
+<link rel="stylesheet" href="src/styles/style.css">
   </style>
     </head>
     <body>
