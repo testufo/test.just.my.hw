0faa5b38833469b9b4d5bb0b9c67639caebe9cd1
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 12:54:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
     <head>
     <title> Головна </title>
+<meta http-equiv="Cache-Control" content="public" />
     <link href="src/styles/style.css" rel="stylesheet" type="text/css" defer>
       <script src="https://polyfill.io/v3/polyfill.min.js?features=es6" defer></script>
   <script id="MathJax-script" defer
