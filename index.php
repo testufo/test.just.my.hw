37c8b72ad99a6fc36a6e5c5e74ac72d5d78c98f8
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-08 16:12:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
     <head>
     <title> Головна </title>
+    <link rel="stylesheet" href="src/styles/style.css">
     </head>
     <body>
         <div class="content">
             <h2 align=center>Тест з математики</h2>
             <img align=right style="margin-right:10%;" width="400" height="200" src="src/images/ds_461390782.jpg" loading="lazy"></img>
-            <link rel="stylesheet" href="src/styles/style.css">
             <form  id="math-form" action="https://q1p.herokuapp.com/result.php" >
                 <p><b>Ім'я</b></p>
                 <input type="text" name="firstname" required pattern="[А-Я]{1}[а-я]{2,20}"> 
