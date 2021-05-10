5bf36c0a0225c24c7a63997f098b92046cd8c3aa
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 13:46:30 +0300

    Update milotest.php

--- milotest.php
+++ milotest.php
@@ -3,11 +3,10 @@ require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
 // First, instantiate the SDK with your API credentials
-$mg = Mailgun::create('4ff3e607f0b0f51202b766d287a900a3-602cc1bf-ece26f0a', 'https://api.mailgun.net/v3/sandbox252a4ec668564d6296482bda252a3024.mailgun.org'); // For EU servers
-
+$mg = Mailgun::create('4ff3e607f0b0f51202b766d287a900a3-602cc1bf-ece26f0a'); 
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
-$mg->messages()->send('sandbox252a4ec668564d6296482bda252a3024.mailgun.org', [
+$mg->messages()->send('valentyn.tk', [
   'from'    => 'bob@sandbox252a4ec668564d6296482bda252a3024.mailgun.org',
   'to'      => 'dd4dd5.yv@gmail.com',
   'subject' => 'The PHP SDK is awesome!',
