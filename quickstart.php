1bcb2aaed3b3d92e35a29c898cee2eeabb940b01
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 14:54:40 +0300

    Update quickstart.php

--- quickstart.php
+++ quickstart.php
@@ -1,9 +1,6 @@
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
-  require 'PHPMailer-master/src/Exception.php';
-  require 'PHPMailer-master/src/PHPMailer.php';
-  require 'PHPMailer-master/src/SMTP.php';
 
   $mail = new PHPMailer();
   $mail->IsSMTP();
