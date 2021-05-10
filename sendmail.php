585ea0fb8afada549e030719e8f9a3a8c8e1eddd
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 15:22:36 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -34,7 +34,7 @@ function sendmail($id, $mail){
 
 <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
   <!--100% body table-->
-  <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+  <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: \'Open Sans\', sans-serif;">
     <tr>
       <td>
         <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
@@ -56,7 +56,7 @@ function sendmail($id, $mail){
                 </tr>
                 <tr>
                   <td style="padding:0 35px;">
-                    <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Дякую за заповнення форми!</h1>
+                    <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:\'Rubik\',sans-serif;">Дякую за заповнення форми!</h1>
                     <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
                     <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
 Ви завжди можете переглянути свій результат тому, що він надійно збрежений у базы данних PostgreSQL : )
