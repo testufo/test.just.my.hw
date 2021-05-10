6ffe6cb7e1901979656e1792311a190c7beaf7a3
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 13:47:38 +0300

    Update milotest.php

--- milotest.php
+++ milotest.php
@@ -6,7 +6,7 @@ use Mailgun\Mailgun;
 $mg = Mailgun::create('4ff3e607f0b0f51202b766d287a900a3-602cc1bf-ece26f0a'); 
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
-$mg->messages()->send('valentyn.tk', [
+$mg->messages()->send('sandbox252a4ec668564d6296482bda252a3024.mailgun.org', [
   'from'    => 'bob@sandbox252a4ec668564d6296482bda252a3024.mailgun.org',
   'to'      => 'dd4dd5.yv@gmail.com',
   'subject' => 'The PHP SDK is awesome!',
