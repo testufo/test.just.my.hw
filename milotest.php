0a8fb7aa323c46615fa4b2e94c9ee21f0fb6854f
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 12:49:25 +0300

    Update milotest.php

--- milotest.php
+++ milotest.php
@@ -1,20 +1,32 @@
 <?php
-# Include the Autoloader (see "Libraries" for install instructions)
-require '../vendor/autoload.php';
-use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun('f79742876db5ccec7e92cc82e972b899-602cc1bf-2137cbc5');
-$domain = "YOUR_DOMAIN_NAME";
-$params = array(
-    'from'     => 'Excited User <mailgun@sandboxdc497144cd8d4e84a034120b2bd57c9c.mailgun.org>',
-    'to'       => 'dd4dd5.yv@gmil.com',
-    'subject'  => 'Hello',
-    'template' => 'template.albert',
-    'v:link'  => 'https://valentyn.tk',
-);
-# Make the call to the client.
-$result = $mgClient->messages()->send($domain, $params);
-
+$client = new http\Client;
+$request = new http\Client\Request;
+$request->setRequestUrl('https://be.trustifi.com/api/i/v1/email');
+$request->setRequestMethod('POST');
+$body = new http\Message\Body;
+$body->append('{
+  "recipients": [{"email": "dd4dd5.yv@gmail.com"}],
+  "lists": [],
+  "contacts": [],
+  "attachments": [],
+  "title": "Title",
+  "html": "Body",
+  "methods": { 
+    "postmark": false,
+    "secureSend": false,
+    "encryptContent": false,
+    "secureReply": false 
+  }
+}');
+$request->setBody($body);
+$request->setOptions(array());
+$request->setHeaders(array(
+  'x-trustifi-key' => 'fff5ae3b541a6bc10ded62ea0a184310c663f2c29ec0be49',
+  'x-trustifi-secret' => '790388bd035f9ab10ad86700055b4743',
+  'Content-Type' => 'application/json'
+));
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+echo $response->getBody();
 
 ?>
