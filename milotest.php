aaeddd770163fc7e17abf2322d27034cf9145a5c
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 13:31:01 +0300

    Update milotest.php

--- milotest.php
+++ milotest.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 # Instantiate the client.
 $mgClient = new Mailgun('4ff3e607f0b0f51202b766d287a900a3-602cc1bf-ece26f0a');
-$domain = "YOUR_DOMAIN_NAME";
+$domain = "sandbox252a4ec668564d6296482bda252a3024.mailgun.org";
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
 	'from'	=> 'Excited User <mailgun@sandbox252a4ec668564d6296482bda252a3024.mailgun.org>',
