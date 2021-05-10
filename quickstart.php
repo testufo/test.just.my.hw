5da39506d56cc6b4fed6d4d3ca5b0281ba7c850d
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 15:08:53 +0300

    Update quickstart.php

--- quickstart.php
+++ quickstart.php
@@ -2,7 +2,7 @@
   require 'vendor/autoload.php';
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
-
+function sendmail(){
   $mail = new PHPMailer();
   $mail->IsSMTP();
 
@@ -17,16 +17,78 @@
   $mail->IsHTML(true);
   $mail->AddAddress("dd4dd5.yv@gmail.com");
   $mail->SetFrom("valentynmail.tk@gmail.com", "valentyn.tk");
-  $mail->AddReplyTo("reply-to-email", "reply-to-name");
-  $mail->AddCC("cc-recipient-email", "cc-recipient-name");
-  $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-  $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+  $mail->Subject = "Дякую за заповнення форми!";
+  $content = "<!doctype html>
+<html lang="en-US">
+
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+  <title>Дякую за заповнення форми!</title>
+  <meta name="description" content="Reset Password Email Template.">
+  <style type="text/css">
+    a:hover {
+      text-decoration: underline !important;
+    }
+  </style>
+</head>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+  <!--100% body table-->
+  <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
+      <td>
+        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td style="height:80px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="text-align:center;">
+            </td>
+          </tr>
+          <tr>
+            <td style="height:20px;">&nbsp;</td>
+          </tr>
+          <tr>
+            <td>
+              <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                <tr>
+                  <td style="height:40px;">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td style="padding:0 35px;">
+                    <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Дякую за заповнення форми!</h1>
+                    <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                    <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+Ви завжди можете переглянути свій результат тому, що він надійно збрежений у базы данних PostgreSQL : )
+                    </p>
+                    <a href="javascript:void(0);" style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Переглянути результат!</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="height:40px;">&nbsp;</td>
+                </tr>
+              </table>
+            </td>
+          <tr>
+            <td style="height:20px;">&nbsp;</td>
+          </tr>
+          <td style="height:80px;">&nbsp;</td>
+    </tr>
+  </table>
+  </td>
+  </tr>
+  </table>
+  <!--/100% body table-->
+</body>
+
+</html>";
 
   $mail->MsgHTML($content); 
   if(!$mail->Send()) {
-    echo "Error while sending Email.";
+    return 1;
     var_dump($mail);
   } else {
-    echo "Email sent successfully";
+    return 0;
+  }
   }
 ?>
