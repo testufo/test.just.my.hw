96c2aee6b765bd4a19c2e9737f13f007a64b7cf1
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 16:34:02 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -15,7 +15,7 @@ function sendmail($id, $mail){
   $mail->Password   = "tfAp6BHUKrtn2zK";
 
   $mail->IsHTML(true);
-  $mail->AddAddress($mail);
+  $mail->AddAddress(settype($mail, "string"));
   $mail->SetFrom("valentynmail.tk@gmail.com", "valentyn.tk");
   $mail->Subject = "Дякую за заповнення форми!";
   $content = '<!doctype html>
