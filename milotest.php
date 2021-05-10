926988611564148b47b922b71f9cb9dfb7dd22f1
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-10 12:53:08 +0300

    Update milotest.php

--- milotest.php
+++ milotest.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 $client = new http\Client;
 $request = new http\Client\Request;
 $request->setRequestUrl('https://be.trustifi.com/api/i/v1/email');
