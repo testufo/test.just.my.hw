faad1f9da167bfbb033b1cbd4411f7cfa70f95ba
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-12 23:01:51 +0300

    Update complete.php

--- complete.php
+++ complete.php
@@ -93,7 +93,7 @@
 <html>
     <head>
     <title>Результат</title>
-    <link href="/src/styles/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     </head>
     <body>
         <div class="content">
