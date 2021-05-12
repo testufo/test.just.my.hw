967b6617f43d099f37db0a8d0166f1ce8cfc8686
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-12 17:23:39 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,8 @@
     function insert($password, $emai, $firstname, $lastname, $jsonanswer, $mark, $groop){
         $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130");
         $query = "SELECT EXISTS (SELECT * FROM htmltestform WHERE email='$emai')";
-        $exist = pg_fetch_row(pg_query($query))[0];
+        $result = pg_query($query);
+        $exist = pg_fetch_row($result)[0];
         if(!$exist){
             $query = "INSERT INTO htmltestform(pass, firstname, lastname, email, jsonanswer, mark, groop) VALUES
                     ('$password', '$firstname', '$lastname', '$emai', '$jsonanswer', '$mark', '$groop') RETURNING id";
