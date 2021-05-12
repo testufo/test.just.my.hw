63c2d0f88973af86bed00487bec9c0735c3eca43
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-12 18:06:57 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,7 +3,7 @@
         $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130");
 
             $query = "INSERT INTO htmltestform(pass, firstname, lastname, email, jsonanswer, mark, groop) VALUES
-                    ('$password', '$firstname', '$lastname', '$emai', '$jsonanswer', '$mark', '$groop') ON CONFLICT RETURN false RETURNING id";
+                    ('$password', '$firstname', '$lastname', '$emai', '$jsonanswer', '$mark', '$groop') RETURNING id";
             $id = pg_query($query);
         pg_close($dbconn);
         if($id==false){
