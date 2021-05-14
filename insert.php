4bb580008828376c1bd9840d84dbb4c0311249fa
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2021-05-14 11:32:51 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -21,10 +21,10 @@
         return pg_fetch_row($result);
     }
     
-    function getothers($groop){
+    function getothers($groop, $class){
         $others = array();
         $dbconn = pg_connect("host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com dbname=d7qvjv66dimcfg user=nbvnsbswyvclsh password=f47f163b6ecbddaf0f3835b045eb07b1d609c6200269be2bf2716b76ead2b130");   
-        $query = "SELECT firstname, lastname, mark FROM htmltestform WHERE groop=$groop ORDER BY lastname";
+        $query = "SELECT firstname, lastname, mark FROM htmltestform WHERE class='$class' groop=$groop ORDER BY lastname";
         $result = pg_query($query);
         while ($line = pg_fetch_array($result , null, PGSQL_ASSOC)) {
             array_push($others, $line);
